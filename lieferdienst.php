1153c363c10d63c2052342038339e05f98754fb3
Johannes Beichter <jbeichter@gmx.de>
2020-10-02 10:58:37 +0200

    Initialer Import

---
+++ lieferdienst.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Lieferdienst
+ * Plugin URI:      https://github.com/jbeichter/lieferdienst
+ * Description:     Manage simple form orders and their deliveries.
+ * Author:          Johannes Beichter
+ * Author URI:      https://github.com/jbeichter
+ * Text Domain:     lieferdienst
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Lieferdienst
+ */
+
+// Your code starts here.
