4ccc47322c2fed3af9f81657660744cff99ca8c6
Johannes Beichter <jbeichter@gmx.de>
2020-10-11 14:05:58 +0200

    Fehler beim Auflösen von Klassen untersuchen

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -30,7 +30,22 @@ tests_add_filter( 'muplugins_loaded', '_lieferdienst_manually_load_plugin' );
 // Start up the WP testing environment.
 require $_lieferdienst_tests_dir . '/includes/bootstrap.php';
 
+if ( class_exists( 'Lieferdienst\\Inc\\Init' ) ) {
+	echo "class check successful" . PHP_EOL;
+} else {
+	echo "class check NOT successful" . PHP_EOL;
+}
+
 // Initialize class loader.
 if ( file_exists( dirname( dirname( __FILE__ ) ) . '/vendor/autoload.php' ) ) {
+	echo "autoload found" . PHP_EOL;
 	require_once dirname( dirname( __FILE__ ) ) . '/vendor/autoload.php';
+} else {
+	echo "autoload NOT found" . PHP_EOL;
+}
+
+if ( class_exists( 'Lieferdienst\\Inc\\Init' ) ) {
+	echo "class check successful" . PHP_EOL;
+} else {
+	echo "class check NOT successful" . PHP_EOL;
 }
