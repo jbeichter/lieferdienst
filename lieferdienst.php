259856971d35a9cdefcc5ef94b1805c8f0d1a113
Johannes Beichter <jbeichter@gmx.de>
2020-10-14 19:44:01 +0200

    Separater Autoload wieder entfernt

--- lieferdienst.php
+++ lieferdienst.php
@@ -40,8 +40,6 @@ defined( 'ABSPATH' ) || die();
 // Initialize class loader.
 if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 	require_once __DIR__ . '/vendor/autoload.php';
-} else {
-	require_once __DIR__ . '/includes/_autoload/autoload.php';
 }
 
 /**
